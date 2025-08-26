b2577bd560bb59da4800b20147aba4cb48a37715
EliTimeless <132577768+EliTimeless@users.noreply.github.com>
2025-08-26 14:21:51 +0200

    Add favicon and update footer links

--- src/Footer.js
+++ src/Footer.js
@@ -4,14 +4,19 @@ const Footer = () => {
   return (
     <div className="Footer">
       This code is{" "}
-      <a href="https://github.com/EliTimeless/todo-list-react?tab=readme-ov-file">
+      <a
+        href="https://github.com/EliTimeless/notes-app-new"
+        target="_blank"
+        rel="noreferrer"
+      >
         {" "}
         opensourced
       </a>{" "}
       and created by{" "}
       <a
         href="http://linkedin.com/in/eliška-nečasová-front-end-tester
-"
+" target="_blank"
+        rel="noreferrer"
       >
         {" "}
         Eliška Nečasová{" "}
